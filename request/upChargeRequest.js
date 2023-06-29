3e1265b270a4e227123154925cd63dd682edaa65
Mohammad Rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-06-29 10:08:18 +0330

    Update upChargeRequest.js

--- request/upChargeRequest.js
+++ request/upChargeRequest.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 
 const upChargeRequest = async (data, hreq) => {
-  console.log(JSON.stringify(data))
+  JSON.stringify(data.hreq)
+  console.log(data)
   const response = await axios.post(
     `https://thirdparty.dev.tasn.ir/exts/v1/${hreq.hi}/1`,
-    JSON.stringify(data)
+    data
   );
   const responseData = JSON.parse(response.data.hresp);
   console.log(responseData);
