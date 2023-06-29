2ee8f5b517cfaa5c9fdff6fadfce614e4af9acc4
رضائی - محمد <rezaee-moha@tehran.iri>
2023-06-29 19:16:00 +0430

    sign data

--- controller/dataController.js
+++ controller/dataController.js
@@ -5,6 +5,7 @@ const encriptFn = require("../util/encriptFn");
 const createTranId = require("../util/createTranId");
 const upChargeRequest = require("../request/upChargeRequest");
 const decriptFn = require("../util/decriptFn");
+const { json } = require("body-parser");
 exports.userData = async (req, res, next) => {
   const { messageId, timestamp, userId, sign } = req.query;
   //   const rawString = tempString(req.originalUrl);
@@ -23,12 +24,23 @@ exports.userData = async (req, res, next) => {
       hop: 313,
     },
   };
-  const enc = encriptFn(JSON.stringify(upData.hreq));
+
+  console.log(JSON.stringify(JSON.stringify(upData.hreq)));
+
+  const hres =
+    '{"hi":2404,"hop":313,"htime":1688048598,"htran":2023529175317865,"st":1100,"stm":"کلید نامعتبر برای این پذیرنده"}';
+  const resSing =
+    "1#1#JvvEPoSAiZY0AbtjRgT7Z1jvJE4s5T4i45Y54wLwtk8u1IbMZvF5ZBcqcuKwKlJAxnp00u8aSAygn1pNlY4O1bStunWEDCXxPTG2yc9BpggSd2EWO1DHMWwu3lNoawxXrGu2KxHLQGgW8uTgNW7GWLMLZhUWbMZKQmFbEO9liaaRLbh3QRqunWjiz/kbkp9ZK+/g1JPEUWbGe8yZAwUkBW79HyKqAAb6/AAiNO1UvPYnGPvor8w+rTpITUV9thXcohnpyBcHfO9+zYYNKRRUhk5zWoPdB6G602B+aUZ+uOGS522RplR5O0cBRifBplCqV3MmoAwnU0TtOetFgVvnAg==";
+  const enc1 = encriptFn(JSON.stringify(JSON.stringify(hres)).trim());
+  const dec1 = decriptFn(JSON.stringify(JSON.stringify(hres)), resSing);
+  console.log("test : " + dec1);
+
+  const enc = encriptFn(JSON.stringify(JSON.stringify(upData.hreq)).trim());
   console.log("encription : " + enc);
-  const dec = decriptFn(JSON.stringify(upData.hreq), enc);
+  const dec = decriptFn(JSON.stringify(JSON.stringify(upData.hreq)), enc);
   console.log(dec);
 
-  upData.hsign = encriptFn(JSON.stringify(upData.hreq));
+  upData.hsign = encriptFn(JSON.stringify(JSON.stringify(upData.hreq)).trim());
   upData.ver = "1.0.0";
   if (userId) {
     const upResponse = await upChargeRequest(upData, upData.hreq);
--- request/upChargeRequest.js
+++ request/upChargeRequest.js
@@ -8,7 +8,7 @@ const upChargeRequest = async (data, hreq) => {
     hsign: data.hsign,
     ver: data.ver,
   };
-  // console.log(JSON.stringify(newData));
+  console.log(JSON.stringify(newData));
   const response = await axios.post(
     `https://thirdparty.dev.tasn.ir/exts/v1/${hreq.hi}/1`,
     JSON.stringify(newData)
