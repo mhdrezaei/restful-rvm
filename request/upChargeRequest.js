50f5a23e39dfd1bf7e83c42d2e0ba01721913e58
رضائی - محمد <rezaee-moha@tehran.iri>
2023-07-17 07:50:13 +0430

    some change

--- request/upChargeRequest.js
+++ request/upChargeRequest.js
@@ -6,10 +6,10 @@ const upChargeRequest = async (data, hreq) => {
     hsign: data.hsign,
     ver: data.ver,
   };
-  console.log(newData)
+  console.log(JSON.stringify(newData));
   const response = await axios.post(
     `https://thirdparty.dev.tasn.ir/exts/v1/${hreq.hi}/1`,
-    newData
+    JSON.stringify(newData)
   );
   const responseData = JSON.parse(response.data.hresp);
   console.log(response.data);
