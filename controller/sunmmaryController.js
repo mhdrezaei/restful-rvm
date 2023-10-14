e6db73f03af70d95ef04da21932555a50e261d38
Mohammad Rezaei <mhdrezaei990@gmail.com>
2023-10-14 09:29:13 +0330

    fix error in summary controller

--- controller/sunmmaryController.js
+++ controller/sunmmaryController.js
@@ -1,38 +1,38 @@
 const Transaction = require("../models/transaction")
 
 exports.allTransactions = async (req, res, next) => {
-    await Transaction.count({},function( err, count){
+    const count = await Transaction.count({})
         console.log( "Number of transactions:", count );
         res.status(200).json({
             success: true,
             count: count
           });
-    })
+    
 }
 exports.successTransactions = async (req, res, next) => {
-    await Transaction.count({status : "success"},function( err, count){
+    const count = await Transaction.count({status : "success"})
         console.log( "Number of success transactions:", count );
         res.status(200).json({
             success: true,
             count: count
           });
-    })
+    
 }
 exports.failedTransactions = async (req, res, next) => {
-    await Transaction.count({status : "failed"},function( err, count){
+    const count = await Transaction.count({status : "failed"})
         console.log( "Number of failed transactions:", count );
         res.status(200).json({
             success: true,
             count: count
           });
-    })
+    
 }
 exports.unknownTransactions = async (req, res, next) => {
-    await Transaction.count({status : "unknown"},function( err, count){
+    const count = await Transaction.count({status : "unknown"})
         console.log( "Number of unknown transactions:", count );
         res.status(200).json({
             success: true,
             count: count
           });
-    })
+    
 }
